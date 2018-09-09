3a7581e2217c5a8e015211d4c2eb199771909137
Mathew Hall <mhall.js@gmail.com>
2018-09-09 03:01:43 +0100

    Post to cars endpoint added. Get from emojis added

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+var emoji = require('./emoji.json');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -16,5 +18,16 @@ var cars = [
 app.get("/cars", function(req, res) {
     res.send(cars);
 })
+
+app.get("/emoji", function(req, res) {
+    res.send(emoji);
+})
+
+app.post('/cars', function (req, res) {
+    console.log("Car added to cars array: ", req.body.car);
+    cars.push(req.body.car);
+    console.log(cars);
+    res.send(cars);
+  });
 
 app.listen(9000);
